7f9e7869a4a3289f774258208f330952a74e0940
koechMR <briankipkoech328@gmail.com>
2023-07-21 10:52:34 +0300

    new

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,41 @@
 #include "sort.h"
-
 /**
-* insertion_sort_list - Sorts a doubly linked list of integers
-*                       using the insertion sort algorithm.
-* @list: A pointer to the head of a doubly-linked list of integers.
-*
-* Description: Prints the list after each swap.
+* insertion_sort_list - function that sorts a doubly linked list
+*                       of integers in ascending order using the Insertion
+*                       sort algorithm
+* @list: doubly linked list
+* Return: void
 */
+
 void insertion_sort_list(listint_t **list)
 {
-listint_t *iter;
-listint_t *tmp;
+listint_t *node = NULL, *tmp = NULL;
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
-for (iter = (*list)->next; iter != NULL; iter = tmp)
+node = *list;
+node = node->next;
+while (node)
 {
-tmp = iter->next;
-
-while (iter->prev != NULL && iter->n < iter->prev->n)
+while (node->prev && node->n < (node->prev)->n)
 {
-if (iter->prev->prev != NULL)
-iter->prev->prev->next = iter;
-
-if (iter->next != NULL)
-iter->next->prev = iter->prev;
-
-iter->prev->next = iter->next;
-iter->next = iter->prev;
-iter->prev = iter->next->prev;
-iter->next->prev = iter;
-
-if (iter->prev == NULL)
-*list = iter;
-
-print_list((const listint_t *)*list);
+tmp = node;
+if (node->next)
+(node->next)->prev = tmp->prev;
+(node->prev)->next = tmp->next;
+node = node->prev;
+tmp->prev = node->prev;
+tmp->next = node;
+if (node->prev)
+(node->prev)->next = tmp;
+node->prev = tmp;
+if (tmp->prev == NULL)
+*list = tmp;
+print_list(*list);
+node = node->prev;
 }
+node = node->next;
 }
+
 }

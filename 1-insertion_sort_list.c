771c745844e83360cdbe6a7fddcb9e3c4e14b7ed
koechMR <briankipkoech328@gmail.com>
2023-07-21 11:10:11 +0300

    new commit that fixed node that was not available

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,15 +1,34 @@
 #include "sort.h"
 
 /**
+* swap_nodes - Swap two nodes in a listint_t
+* @h: A pointer to the head of the doubly-linked list
+* @n1: A pointer to the first node to swap
+* @n2: The second node to swap
+*/
+void swap_nodes(listint_t **x, listint_t **n1, listint_t *n2)
+{
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+(*n1)->prev->next = n2;
+else
+*x = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
+}
+
+/**
 * insertion_sort_list - Sorts a doubly linked list of integers
-*                       using the insertion sort algorithm.
-* @list: A pointer to the head of a doubly-linked list of integers.
-*
-* Description: Prints the list after each swap.
+* @list: A pointer to the head of a doubly-linked list
+* Description: Prints the list after each swap
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *iter, *tmp;
+listint_t *iter, *insert, *tmp;
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
@@ -17,20 +36,10 @@ return;
 for (iter = (*list)->next; iter != NULL; iter = tmp)
 {
 tmp = iter->next;
-
-while (iter->prev != NULL && iter->n < iter->prev->n)
+insert = iter->prev;
+while (insert != NULL && iter->n < insert->n)
 {
-iter->prev->next = iter->next;
-if (iter->next != NULL)
-iter->next->prev = iter->prev;
-
-iter->next = iter->prev;
-iter->prev = iter->prev->prev;
-iter->next->prev = iter;
-
-if (iter->prev == NULL)
-*list = iter;
-
+swap_nodes(list, &insert, iter);
 print_list((const listint_t *)*list);
 }
 }
